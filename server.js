96fe333c58fadeab55c44e00185dc61986a16f5b
javan <javanluiz757@gmail.com>
2024-08-15 17:09:57 -0300

    utilização do studio prisma

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    console.log("tentando");
+    await prisma.$connect();
+    console.log("conseguiu");
+    prisma.user.create({
+      data: { age: 21, email: "javanlsj@gmail.com", name: "Javan" },
+    });
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 const app = express();
 app.use(express.json());
 
-app.post("/usuarios", (req, res) => {
-  console.log(req.body);
-  res.send("o post deu bom");
-});
+const users = [];
 
+app.post("/usuarios", (req, res) => {
+  users.push(req.body);
+
+  res.status(201).json(req.body);
+});
 app.get("/usuarios", (req, res) => {
-  res.send("Deu bom!");
+  res.status(200).json(users);
 });
 
 app.listen(3000, () => {
